40e5563f9eca246b7fe822c5c6088e5345d1d573
Noemi Glaeser <noemi@lanai.local>
2016-02-20 02:12:58 -0500

    Added documentation, removed "return to previous sequence?" question, modified program to only print average I.C. table instead of all I.C.'s for all sequences

--- Glaeser_Program1.java
+++ Glaeser_Program1.java
@@ -1,81 +1,131 @@
-import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
+/***********************************************************************************
+ * Cryptography (CSCE 557) – Spring 2016
+ * Programming Assignment #1: Vigenère cipher
+ * Author/copyright: Noemi Glaeser
+ * Last modified: 19 February 2016
+ *
+ * This program analyzes a ciphertext, assuming it is encrypted using a Vigenère,
+ * in two steps:
+ * (1) First the program determines the probable key length using the index of
+ *     coincidence method (http://practicalcryptography.com/cryptanalysis/
+ *     stochastic-searching/cryptanalysis-vigenere-cipher/)
+ * (2) Using a key length inputted by the user, each monoalphabetic sequence
+ *     in the ciphertext is analyzed separately. Frequencies of each ciphertext
+ *     letter are outputted and the user can test different shifts before accepting
+ *     one and moving on to the next sequence in the ciphertext.
+ *
+ * Input: Ciphertext, inputted by user into the console
+ *
+ * Output: Index of coincidence (I.C.), calculated for every key length (up to a
+ *            user-specified max)
+ *         Letter frequencies for every monoalphabetic sequence in the ciphertext
+ *         Partially decrypted ciphertext after each monoalph sequence substitution
+ *            (following convention, ciphertext letters are uppercase, plaintext
+ *            lowercase)
+ *
+ *         Missing (possibly to add in future revisions):
+ *            Vigenère key the ciphertext employed
+ *
+ * Note: In this program, the word 'sequence' refers to the multiple monoalphabetic
+ *       components of the ciphertext.
+ *       i.e., for a key length of 3, the ciphertext consists of 3 monoalphabetic
+ *       sequences of characters at positions
+ *       1, 4, 7, 10, 13, 16, ...
+ *       2, 5, 8, 11, 14, 17, ...
+ *       3, 6, 9, 12, 15, 18, ...
+ */
+
+// to do: "which character would you like to use as 'e'?" should only take one character, not a whole word
+//        organize code with more methods
+//        print key
+
+// Ciphertext provided for this assignment:
+// xkjurowmllpxwznpimbvbqjcnowxpcchhvvfvsllfvxhazityxohulxqojaxelxzxmyjaqfstsrulhhucdskbxknjqidallpqslluhiaqfpbpcidsvcihwhwewthbtxrljnrsncihuvffuxvoukjljswmaqfvjwjsdyljogjxdboxajultucpzmpliwmlubzxvoodybafdskxgqfadshxnxehsaruojaqfpfkndhsaafvulluwtaqfrupwjrszxgpfutjqiynrxnyntwmhcukjfbirzsmehhsjshyonddzzntzmplilrwnmwmlvuryonthuhabwnvw
 
 import java.util.*;
 
-//Noemi Glaeser
-//Cryptography - Spring 2016
-
 public class Glaeser_Program1 {
     public static final String ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner keybd = new Scanner(System.in);
         System.out.print("Type ciphertext here:");
         String str = keybd.nextLine().toUpperCase();
 
+        //break ciphertext into characters in a character array
         char[] ciphertext = parse(str);
 
         System.out.println("\nWhat max length of key would you like to check?");
-        int n = keybd.nextInt(); //max length of key
-        System.out.print("Original: ");
+        int n = keybd.nextInt();
+
+        System.out.print("\nOriginal: ");
         for(char c : ciphertext)
             System.out.print(c);
         System.out.println();
 
+        /****** PART 1: FINDING THE KEY LENGTH *******/
+
+        //create array to store I.C. for every key length up to max
         double[] icavgs = new double[n];
 
-        //find key length using index of coincidence method (http://practicalcryptography.com/cryptanalysis/stochastic-searching/cryptanalysis-vigenere-cipher/)
-        for(int k = 2; k <= n; k++) //start guessing a key length 2 and go up to the inputted key length
+        //calculate I.C. for each key length – start at 2 and go up to max
+        //see "Glaeser_homework1.pdf" for the formula used
+        for(int k = 2; k <= n; k++)
         {
-            System.out.println();
-            System.out.println("\n***Key length: " + k + "***");
-
             double avg = 0.0;
-            for(int i = 0; i < k; i++) //in order to do multiples and break up ciphertext (k is the current key length attempted)
+            //variable i is used to iterate through the ciphertext in multiples
+            //to break the ciphertext into sequences
+            for(int i = 0; i < k; i++)
             {
-                System.out.print("\nSequence " + (i+1) + ": ");
                 double ic = 0.0;
-                int a = 0; //to count length of the sequence
-                for(int x = i; x < ciphertext.length; x = x+k) //first, print out the sequence
-                {
-                    System.out.print(ciphertext[x]); //prints sequence
+                int a = 0; //length of the sequence
+
+                //first, we count the number of letters in the sequence
+                for(int x = i; x < ciphertext.length; x = x+k)
                     a++;
-                }
+
                 for(char c = 'A'; c <= 'Z'; c++) {
                     int fi = 0; //value fi for each character, to be summed
-                    for (int x = i; x < ciphertext.length; x = x + k) //same loop but to find the i.c.
+                    //loop to calculate the I.C.
+                    for (int x = i; x < ciphertext.length; x = x + k)
                     {
-                        if(ciphertext[x] == c) //if the character is the one we're iterating for rn, incr count of that char
+                        //if the current character is the one we're iterating for, increase its count
+                        if(ciphertext[x] == c)
                             fi++;
                     }
-                    ic += (fi * (fi - 1)); //sum up the expression for every char a through z
+                    //I.C. = sum of this expression for every char A through Z
+                    ic += (fi * (fi - 1));
                 }
-                ic = ic/(a * (a-1)); //calculated i.c. for this sequence
-                System.out.print("\tI.C.: " + ic);
+                //calculated I.C. for this sequence
+                ic = ic/(a * (a-1));
                 avg += ic;
             }
+            //Average the I.C. of every sequence at this key length
             avg = avg/k;
-            System.out.println("\tAverage: " + avg);
 
-            icavgs[k - 1] = avg; //put avg i.c.'s in a table (each at index key length - 1
+            //put avg I.C.'s in a table (each at index (key length-1))
+            icavgs[k - 1] = avg;
         }
 
-        printtable(icavgs); //print the avg i.c.'s for each key length in a table
+        printtable(icavgs);
 
+        //Tell the user at which key length the maximum I.C. occurs
         System.out.println("Max I.C. = " + max(icavgs));
 
-        //now, decode ciphertext
+        /****** PART 2: DECODING THE CIPHERTEXT *******/
+
         System.out.println("What key length would you like to attempt to crack with?");
         int keylength = keybd.nextInt();
 
         int[] key = new int[keylength];
-        //get input from keyboard for key length to try
-        //frequency analysis for each nth letter
 
         String seq = "";
         String curr = "";
         int k = 0;
+        //work with each sequence in turn
         while(k < key.length)
         {
+            //build sequence to analyze
             if(k < key.length) {
                 System.out.print("Sequence " + (k+1) + ": ");
                 seq = "";
@@ -86,16 +136,19 @@ public class Glaeser_Program1 {
                     i += key.length;
                 } while (i < ciphertext.length);
             }
-            printfreq(seq); //prints out char freq of the sequence
+            //print letter frequencies in the sequence
+            printfreq(seq);
 
             System.out.println("Which character would you like to use as \'e\'?");
-            try{
-                char c = keybd.next().charAt(0);
+            try
+            {
+                char c = Character.toLowerCase(keybd.next().charAt(0));
+                //plaintext version of the current sequence
                 String ptxtseq = replaceShiftE(seq, c);
 
-                //print the full ciphertext, replaced
+                //print the partially decrypted ciphertext (named "curr") with this sequence decrypted
                 //when you hit a multiple of k, replace with the next char from the ptxtseq string
-                curr = ""; //new partially decrypted ciphertext
+                curr = "";
                 for(int m = 0; m < ciphertext.length; m++)
                 {
                     if((m - k) % 5 == 0) {
@@ -108,29 +161,16 @@ public class Glaeser_Program1 {
                 System.out.println(curr);
 
                 System.out.println("Accept replacement? y/n");
-                try {
-                    if(keybd.next().equalsIgnoreCase("y")) {
+                try
+                {
+                    if(keybd.next().equalsIgnoreCase("y"))
                         ciphertext = curr.toCharArray();
-                        //break;
-                    }
-                    //if no, it'll just loop through choosing an 'e' again
-                    else {
-                        System.out.println("Want to return to the previous sequence? y/n");
-                        while(true) {
-                            try {
-                                if (keybd.next().equalsIgnoreCase("y")) {
-                                    //System.out.println("Enter sequence number (1-" + (k+1) + "): ");
-                                    k--;
-                                    break;
-                                }
-                                break;
-                            }
-                            catch (InputMismatchException e) {
-                                System.out.println("enter y or n");
-                            }
-                        }
-                    }
-                } catch (InputMismatchException e) {
+                    //if no, loop through choosing an 'e' again
+                    else
+                        k--;
+                }
+                catch (InputMismatchException e)
+                {
                     System.out.println("enter y or n");
                 }
 
@@ -156,7 +196,7 @@ public class Glaeser_Program1 {
         for(char c = 'A'; c <= 'Z'; c++){ //ciphertext chars to be replaced
             //index of E - ch determines shift
             //4 - index of ch = shift
-            // ( index of currect letter c + shift ) %26 = plaintext
+            // ( index of current letter 'c' + shift ) %26 = plaintext index
             //%26 might return a negative
             int i = (ABC.indexOf(c) + shift)%26; //index of the plaintext character
             if(i < 0)
@@ -166,16 +206,18 @@ public class Glaeser_Program1 {
         return s;
     }
 
-    //print frequences of characters of each sequence
+    //print frequencies of characters of each sequence
     public static void printfreq(String s)
     {
-        int[] freq = new int[26]; //to store the letter frequencies of this sequence
-        freq = clear(freq); //fill it with zeros
+        //store letter frequencies
+        int[] freq = new int[26];
+        freq = clear(freq);
 
-        for(char c = 'A'; c <= 'Z'; c++) { //count character frequencies
-            for (int x = 0; x < s.length(); x++) //loop through the string
+        for(char c = 'A'; c <= 'Z'; c++)
+        {
+            for (int x = 0; x < s.length(); x++)
             {
-                if(s.charAt(x) == c) //if the character is the one we're checking for rn, incr count of that char
+                if(s.charAt(x) == c)
                     freq[ABC.indexOf(c)]++;
             }
         }
@@ -183,6 +225,7 @@ public class Glaeser_Program1 {
         for(int i = 0; i < freq.length; i++)
         {
             System.out.print(ABC.charAt(i) + ":\t" + freq[i] + "\t");
+            //create a bar-graph-like representation of frequency with asterisks
             for(int k = 0; k < freq[i]; k++)
             {
                 System.out.print("*");
@@ -225,13 +268,12 @@ public class Glaeser_Program1 {
     public static char[] shorten(char[] c, int n)
     {
         char[] ret = new char[n];
-        for(int i = 0; i < n; i++) //copy every element from c into ret up to n
+        for(int i = 0; i < n; i++)
             ret[i] = c[i];
-
         return ret;
     }
 
-    //find maximum I.C.
+    //find and print maximum I.C.
     public static String max(double[] d)
     {
         double max = 0.0;
